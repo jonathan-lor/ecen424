d0f055dc96cc40dbe549ee320bd5c0b2eb9c5bdf
Jonathan Lor <jonathan.r.lor@gmail.com>
2024-10-02 19:29:27 -0500

    test

--- mp2/ConcurrentMatMul.java
+++ mp2/ConcurrentMatMul.java
@@ -11,7 +11,8 @@ public class ConcurrentMatMul {
 		m1 = new ArrayList<ArrayList<Integer>>(m1Dimensions);
 		m2 = new ArrayList<ArrayList<Integer>>(m2Dimensions);
 		
-		
+		populateMatrices(m1, m1Dimensions, m1Dimensions);
+		populateMatrices(m2, m2Dimensions, m2Dimensions);
 	}
 
 	private void populateMatrices(ArrayList<ArrayList<Integer>> m, int rows, int cols) {
@@ -19,21 +20,40 @@ public class ConcurrentMatMul {
 		for(int i = 0; i < rows; i++) {
 			ArrayList<Integer> row = new ArrayList<Integer>(cols);
 			for(int j = 0; j < cols; j++) {
-				row.set(j, rand.nextInt(10));
+				row.add(j, rand.nextInt(10));
 			}
 			m.set(i, row);
 		}
 	}
+	
 
 	public ConcurrentMatMul(int m1Dimensions, int m2Dimensions) {
 		this.m1Dimensions = m1Dimensions;
 		this.m2Dimensions = m2Dimensions;
+		this.generateMatrices();
 	}
 
 	public ConcurrentMatMul() {
 		this.m1Dimensions = 20;
 		this.m2Dimensions = 20;
+		this.generateMatrices();
 	}
-
 	
+	public void printMatrices() {
+		System.out.println("Matrix 1:");
+		for(int i = 0; i < m1Dimensions; i++) {
+			for(int j = 0; j < m1Dimensions; j++) {
+				System.out.print(m1.get(i).get(j) + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println("Matrix 2:");
+		for(int i = 0; i < m2Dimensions; i++) {
+			for(int j = 0; j < m2Dimensions; j++) {
+				System.out.print(m2.get(i).get(j) + " ");
+			}
+			System.out.println("");
+		}
+
+	}
 }
--- mp2/Main.java
+++ mp2/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		
+		System.out.println("Hello from main");
+		ConcurrentMatMul ccmm = new ConcurrentMatMul();
+		ccmm.printMatrices();		
 	}
 }
